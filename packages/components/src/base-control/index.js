8e4ab9bf51bb5d344ea43ce1f6f14b436688bb2a
Jon Q <hello@jonquach.com>
2020-01-06 15:46:09 -0500

    Add isLoading support to BaseControl component

--- packages/components/src/base-control/index.js
+++ packages/components/src/base-control/index.js
@@ -6,39 +6,69 @@ import classnames from 'classnames';
 /**
  * Internal dependencies
  */
+import Spinner from '../spinner';
 import VisuallyHidden from '../visually-hidden';
 
-function BaseControl( { id, label, hideLabelFromVision, help, className, children } ) {
+function BaseControl( {
+	id,
+	isLoading = false,
+	label,
+	hideLabelFromVision,
+	help,
+	className,
+	children,
+} ) {
+	const classes = classnames(
+		'components-base-control',
+		isLoading && 'is-loading',
+		className
+	);
+
+	const helpId = `${ id }-help`;
+
 	return (
-		<div className={ classnames( 'components-base-control', className ) }>
+		<div className={ classes } aria-busy={ isLoading }>
 			<div className="components-base-control__field">
-				{ label && id && ( hideLabelFromVision ?
-					<VisuallyHidden
-						as="label"
-						htmlFor={ id }>{ label }</VisuallyHidden> :
-					<label
-						className="components-base-control__label"
-						htmlFor={ id }>{ label }</label>
-				) }
-				{ label && ! id && ( hideLabelFromVision ?
-					<VisuallyHidden
-						as="label">{ label }</VisuallyHidden> :
-					<BaseControl.VisualLabel>{ label }</BaseControl.VisualLabel>
-				) }
-				{ children }
+				{ label &&
+					id &&
+					( hideLabelFromVision ? (
+						<VisuallyHidden as="label" htmlFor={ id }>
+							{ label }
+						</VisuallyHidden>
+					) : (
+						<label
+							className="components-base-control__label"
+							htmlFor={ id }
+						>
+							{ label }
+						</label>
+					) ) }
+				{ label &&
+					! id &&
+					( hideLabelFromVision ? (
+						<VisuallyHidden as="label">{ label }</VisuallyHidden>
+					) : (
+						<BaseControl.VisualLabel>
+							{ label }
+						</BaseControl.VisualLabel>
+					) ) }
+				<div className="components-base-control__field-content">
+					{ children }
+					{ isLoading && <Spinner /> }
+				</div>
 			</div>
-			{ !! help && <p id={ id + '__help' } className="components-base-control__help">{ help }</p> }
+			{ !! help && (
+				<p id={ helpId } className="components-base-control__help">
+					{ help }
+				</p>
+			) }
 		</div>
 	);
 }
 
 BaseControl.VisualLabel = ( { className, children } ) => {
 	className = classnames( 'components-base-control__label', className );
-	return (
-		<span className={ className }>
-			{ children }
-		</span>
-	);
+	return <span className={ className }>{ children }</span>;
 };
 
 export default BaseControl;
