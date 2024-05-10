fc7fce370cc382ebc9785b42e77ed73f177b779a
Ella <ella@vandurpe.com>
2024-05-10 10:41:55 +0900

    Fix label on open

--- packages/block-editor/src/components/inserter/menu.js
+++ packages/block-editor/src/components/inserter/menu.js
@@ -33,6 +33,7 @@ import useInsertionPoint from './hooks/use-insertion-point';
 import { store as blockEditorStore } from '../../store';
 import TabbedSidebar from '../tabbed-sidebar';
 import { useZoomOut } from '../../hooks/use-zoom-out';
+import { usePatternCategories } from './block-patterns-tab/use-pattern-categories';
 import { unlock } from '../../lock-unlock';
 
 const NOOP = () => {};
@@ -67,9 +68,12 @@ function InserterMenu(
 	const [ filterValue, setFilterValue, delayedFilterValue ] =
 		useDebouncedInput( __experimentalFilterValue );
 	const [ hoveredItem, setHoveredItem ] = useState( null );
-	const [ selectedPatternCategory, setSelectedPatternCategory ] = useState( {
-		name: __experimentalInitialCategory,
-	} );
+	const categories = usePatternCategories( rootClientId );
+	const [ selectedPatternCategory, setSelectedPatternCategory ] = useState(
+		categories.find(
+			( category ) => category.name === __experimentalInitialCategory
+		)
+	);
 	const [ patternFilter, setPatternFilter ] = useState( 'all' );
 	const [ selectedMediaCategory, setSelectedMediaCategory ] =
 		useState( null );
@@ -251,6 +255,7 @@ function InserterMenu(
 				onInsert={ onInsertPattern }
 				onSelectCategory={ onClickPatternCategory }
 				selectedCategory={ selectedPatternCategory }
+				categories={ categories }
 			>
 				{ showPatternPanel && (
 					<PatternCategoryPreviews
