a869a793040338f5d3893f0918c7ff20142e8609
Marco Ciampini <marco.ciampo@gmail.com>
2024-07-15 15:53:48 +0200

    Remove Composite from private APIs

--- packages/components/src/private-apis.ts
+++ packages/components/src/private-apis.ts
@@ -1,7 +1,6 @@
 /**
  * Internal dependencies
  */
-import { Composite, useCompositeStore } from './composite';
 import { positionToPlacement as __experimentalPopoverLegacyPositionToPlacement } from './popover/utils';
 import { createPrivateSlotFill } from './slot-fill';
 import {
@@ -22,11 +21,6 @@ import { lock } from './lock-unlock';
 
 export const privateApis = {};
 lock( privateApis, {
-	CompositeV2: Composite,
-	CompositeGroupV2: Composite.Group,
-	CompositeItemV2: Composite.Item,
-	CompositeRowV2: Composite.Row,
-	useCompositeStoreV2: useCompositeStore,
 	__experimentalPopoverLegacyPositionToPlacement,
 	createPrivateSlotFill,
 	ComponentsContext,
