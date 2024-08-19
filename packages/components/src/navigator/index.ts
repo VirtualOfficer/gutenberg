6a288c5db2ea7d3c85034c7ef83a99b35e43d69f
Marco Ciampini <marco.ciampo@gmail.com>
2024-08-19 15:02:19 +0200

    Export overloaded notation

--- packages/components/src/navigator/index.ts
+++ packages/components/src/navigator/index.ts
@@ -1,6 +1,21 @@
-export { NavigatorProvider } from './navigator-provider';
-export { NavigatorScreen } from './navigator-screen';
-export { NavigatorButton } from './navigator-button';
-export { NavigatorBackButton } from './navigator-back-button';
-export { NavigatorToParentButton } from './navigator-to-parent-button';
+/**
+ * Internal dependencies
+ */
+import { Navigator as NavigatorProvider } from './navigator';
+import { NavigatorScreen } from './navigator-screen';
+import { NavigatorButton } from './navigator-button';
+import { NavigatorBackButton } from './navigator-back-button';
 export { useNavigator } from './use-navigator';
+
+export const Navigator = Object.assign( NavigatorProvider, {
+	dislayName: 'Navigator',
+	Screen: Object.assign( NavigatorScreen, {
+		displayName: 'Navigator.Screen',
+	} ),
+	Button: Object.assign( NavigatorButton, {
+		displayName: 'Navigator.Button',
+	} ),
+	BackButton: Object.assign( NavigatorBackButton, {
+		displayName: 'Navigator.BackButton',
+	} ),
+} );
