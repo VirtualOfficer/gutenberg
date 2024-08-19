4a66808baba0a5cdb74623ec1707ae4a343b786a
Marco Ciampini <marco.ciampo@gmail.com>
2024-08-19 13:59:09 +0200

    Move legacy exports to separate file

---
+++ packages/components/src/navigator/legacy.ts
@@ -0,0 +1,6 @@
+export { Navigator as NavigatorProvider } from './navigator';
+export { NavigatorScreen } from './navigator-screen';
+export { NavigatorButton } from './navigator-button';
+export { NavigatorBackButton } from './navigator-back-button';
+export { NavigatorToParentButton } from './navigator-to-parent-button';
+export { useNavigator } from './use-navigator';
