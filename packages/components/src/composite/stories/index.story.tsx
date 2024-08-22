6df5ca97f78a7a64a3e35029c70e16ed1b1dddc1
Marco Ciampini <marco.ciampo@gmail.com>
2024-08-22 17:24:19 +0200

    Update and simplify Storybook

--- packages/components/src/composite/stories/index.story.tsx
+++ packages/components/src/composite/stories/index.story.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 /**
  * WordPress dependencies
@@ -12,15 +12,11 @@ import { isRTL } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { Composite } from '..';
-import { useCompositeStore } from '../store';
-import { UseCompositeStorePlaceholder, transform } from './utils';
 
-const meta: Meta< typeof UseCompositeStorePlaceholder > = {
+const meta: Meta< typeof Composite > = {
 	title: 'Components/Composite (V2)',
-	component: UseCompositeStorePlaceholder,
+	component: Composite,
 	subcomponents: {
-		// @ts-expect-error - See https://github.com/storybookjs/storybook/issues/23170
-		Composite,
 		// @ts-expect-error - See https://github.com/storybookjs/storybook/issues/23170
 		'Composite.Group': Composite.Group,
 		// @ts-expect-error - See https://github.com/storybookjs/storybook/issues/23170
@@ -35,6 +31,8 @@ const meta: Meta< typeof UseCompositeStorePlaceholder > = {
 		'Composite.Typeahead': Composite.Typeahead,
 	},
 	argTypes: {
+		children: { control: { type: null } },
+		render: { control: { type: null } },
 		setActiveId: { control: { type: null } },
 		focusLoop: {
 			control: 'select',
@@ -50,7 +48,6 @@ const meta: Meta< typeof UseCompositeStorePlaceholder > = {
 		controls: { expanded: true },
 		docs: {
 			canvas: { sourceState: 'shown' },
-			source: { transform },
 		},
 	},
 	decorators: [
@@ -91,116 +88,112 @@ const meta: Meta< typeof UseCompositeStorePlaceholder > = {
 };
 export default meta;
 
-export const Default: StoryFn< typeof UseCompositeStorePlaceholder > = (
-	storeProps
-) => {
-	const rtl = isRTL();
-	const store = useCompositeStore( { rtl, ...storeProps } );
-
-	return (
-		<Composite store={ store }>
-			<Composite.Item>Item one</Composite.Item>
-			<Composite.Item>Item two</Composite.Item>
-			<Composite.Item>Item three</Composite.Item>
-		</Composite>
-	);
+export const Default: StoryObj< typeof Composite > = {
+	args: {
+		rtl: isRTL(),
+		children: (
+			<>
+				<Composite.Item>Item one</Composite.Item>
+				<Composite.Item>Item two</Composite.Item>
+				<Composite.Item>Item three</Composite.Item>
+			</>
+		),
+	},
 };
 
-export const Groups: StoryFn< typeof UseCompositeStorePlaceholder > = (
-	storeProps
-) => {
-	const rtl = isRTL();
-	const store = useCompositeStore( { rtl, ...storeProps } );
-
-	return (
-		<Composite store={ store }>
-			<Composite.Group>
-				<Composite.GroupLabel>Group one</Composite.GroupLabel>
-				<Composite.Item>Item 1.1</Composite.Item>
-				<Composite.Item>Item 1.2</Composite.Item>
-			</Composite.Group>
-			<Composite.Group>
-				<Composite.GroupLabel>Group two</Composite.GroupLabel>
-				<Composite.Item>Item 2.1</Composite.Item>
-				<Composite.Item>Item 2.1</Composite.Item>
-			</Composite.Group>
-		</Composite>
-	);
+export const Groups: StoryObj< typeof Composite > = {
+	...Default,
+	args: {
+		children: (
+			<>
+				<Composite.Group>
+					<Composite.GroupLabel>Group one</Composite.GroupLabel>
+					<Composite.Item>Item 1.1</Composite.Item>
+					<Composite.Item>Item 1.2</Composite.Item>
+				</Composite.Group>
+				<Composite.Group>
+					<Composite.GroupLabel>Group two</Composite.GroupLabel>
+					<Composite.Item>Item 2.1</Composite.Item>
+					<Composite.Item>Item 2.1</Composite.Item>
+				</Composite.Group>
+			</>
+		),
+	},
 };
 
-export const Grid: StoryFn< typeof UseCompositeStorePlaceholder > = (
-	storeProps
-) => {
-	const rtl = isRTL();
-	const store = useCompositeStore( { rtl, ...storeProps } );
-
-	return (
-		<Composite role="grid" store={ store } aria-label="Composite">
-			<Composite.Row role="row">
-				<Composite.Item role="gridcell">Item A1</Composite.Item>
-				<Composite.Item role="gridcell">Item A2</Composite.Item>
-				<Composite.Item role="gridcell">Item A3</Composite.Item>
-			</Composite.Row>
-			<Composite.Row role="row">
-				<Composite.Item role="gridcell">Item B1</Composite.Item>
-				<Composite.Item role="gridcell">Item B2</Composite.Item>
-				<Composite.Item role="gridcell">Item B3</Composite.Item>
-			</Composite.Row>
-			<Composite.Row role="row">
-				<Composite.Item role="gridcell">Item C1</Composite.Item>
-				<Composite.Item role="gridcell">Item C2</Composite.Item>
-				<Composite.Item role="gridcell">Item C3</Composite.Item>
-			</Composite.Row>
-		</Composite>
-	);
+export const Grid: StoryObj< typeof Composite > = {
+	...Default,
+	args: {
+		role: 'grid',
+		'aria-label': 'Composite',
+		children: (
+			<>
+				<Composite.Row role="row">
+					<Composite.Item role="gridcell">Item A1</Composite.Item>
+					<Composite.Item role="gridcell">Item A2</Composite.Item>
+					<Composite.Item role="gridcell">Item A3</Composite.Item>
+				</Composite.Row>
+				<Composite.Row role="row">
+					<Composite.Item role="gridcell">Item B1</Composite.Item>
+					<Composite.Item role="gridcell">Item B2</Composite.Item>
+					<Composite.Item role="gridcell">Item B3</Composite.Item>
+				</Composite.Row>
+				<Composite.Row role="row">
+					<Composite.Item role="gridcell">Item C1</Composite.Item>
+					<Composite.Item role="gridcell">Item C2</Composite.Item>
+					<Composite.Item role="gridcell">Item C3</Composite.Item>
+				</Composite.Row>
+			</>
+		),
+	},
 };
 
-export const Hover: StoryFn< typeof UseCompositeStorePlaceholder > = (
-	storeProps
-) => {
-	const rtl = isRTL();
-	const store = useCompositeStore( { rtl, ...storeProps } );
-
-	return (
-		<Composite store={ store }>
-			<Composite.Hover render={ <Composite.Item /> }>
-				Hover item one
-			</Composite.Hover>
-			<Composite.Hover render={ <Composite.Item /> }>
-				Hover item two
-			</Composite.Hover>
-			<Composite.Hover render={ <Composite.Item /> }>
-				Hover item three
-			</Composite.Hover>
-		</Composite>
-	);
-};
-Hover.parameters = {
-	docs: {
-		description: {
-			story: 'Elements in the composite widget will receive focus on mouse move and lose focus to the composite base element on mouse leave.',
+export const Hover: StoryObj< typeof Composite > = {
+	...Default,
+	args: {
+		children: (
+			<>
+				<Composite.Hover render={ <Composite.Item /> }>
+					Hover item one
+				</Composite.Hover>
+				<Composite.Hover render={ <Composite.Item /> }>
+					Hover item two
+				</Composite.Hover>
+				<Composite.Hover render={ <Composite.Item /> }>
+					Hover item three
+				</Composite.Hover>
+			</>
+		),
+	},
+	parameters: {
+		docs: {
+			description: {
+				story: 'Elements in the composite widget will receive focus on mouse move and lose focus to the composite base element on mouse leave.',
+			},
 		},
 	},
 };
 
-export const Typeahead: StoryFn< typeof UseCompositeStorePlaceholder > = (
-	storeProps
-) => {
-	const rtl = isRTL();
-	const store = useCompositeStore( { rtl, ...storeProps } );
-
-	return (
-		<Composite store={ store } render={ <Composite.Typeahead /> }>
-			<Composite.Item>Apple</Composite.Item>
-			<Composite.Item>Banana</Composite.Item>
-			<Composite.Item>Peach</Composite.Item>
-		</Composite>
-	);
-};
-Typeahead.parameters = {
-	docs: {
-		description: {
-			story: 'When focus in on the composite widget, hitting printable character keys will move focus to the next composite item that begins with the input characters.',
+export const Typeahead: StoryObj< typeof Composite > = {
+	args: {
+		render: <Composite.Typeahead />,
+		children: (
+			<>
+				<Composite.Item>Apple</Composite.Item>
+				<Composite.Item>Banana</Composite.Item>
+				<Composite.Item>Peach</Composite.Item>
+			</>
+		),
+	},
+	parameters: {
+		docs: {
+			description: {
+				story: 'When focus in on the composite widget, hitting printable character keys will move focus to the next composite item that begins with the input characters.',
+			},
 		},
 	},
 };
+
+// TODO: example accessing the store
+
+// TODO: example across SlotFill forwarding context
